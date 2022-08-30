5d6bdb6bef31add399e04ecdf7f245f9eb6203ca
[firstname lastname] <[valid-email]>
2022-08-30 16:42:36 +0530

    arrow function reduce

---
+++ find.js
@@ -0,0 +1,8 @@
+const ages = [4,11,23,39];
+
+function checkage(age) {
+
+    return age > 23;
+
+}
+console.log(ages.find(checkage));
---
+++ map.js
@@ -0,0 +1,7 @@
+const numbers = [4,9,36,49];
+const newArr = numbers.map(Math.sqrt);
+
+
+
+console.log(newArr);
+
